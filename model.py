6f68f8aa0651b79b3d63b86e59395a04137633f8
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2022-05-29 01:14:10 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 from heapq import heappop, heappush
 import gym
@@ -20,13 +22,13 @@ class AStar:
     def __init__(self):
         self.start = (0, 0)
         self.goal = (0, 0)
-        self.max_steps = 1000  # due to the absence of information about the map size we need some other stop criterion
+        self.max_steps = 5000  # due to the absence of information about the map size we need some other stop criterion
         self.OPEN = list()
         self.CLOSED = dict()
         self.obstacles = set()
         self.other_agents = set()
 
-    def compute_shortest_path(self, start, goal):
+    def compute_shortest_path(self, start, goal, n=5):
         self.start = start
         self.goal = goal
         self.CLOSED = dict()
@@ -34,13 +36,15 @@ class AStar:
         heappush(self.OPEN, Node(self.start))
         u = Node()
         steps = 0
-        while self.OPEN and steps < self.max_steps and (u.i, u.j) != self.goal:
+        max_steps = min(max(self.max_steps - n * 12, 20), self.max_steps)
+        while self.OPEN and steps < max_steps and (u.i, u.j) != self.goal:
             u = heappop(self.OPEN)
             steps += 1
             for d in {(-1, 0), (1, 0), (0, -1), (0, 1)}:
-                n = (u.i+d[0], u.j + d[1])
+                n = (u.i + d[0], u.j + d[1])
                 if n not in self.obstacles and n not in self.CLOSED and n not in self.other_agents:
-                    h = abs(n[0] - self.goal[0]) + abs(n[1] - self.goal[1])  # Manhattan distance as a heuristic function
+                    h = abs(n[0] - self.goal[0]) + abs(
+                        n[1] - self.goal[1])  # Manhattan distance as a heuristic function
                     heappush(self.OPEN, Node(n, u.g + 1, h))
                     self.CLOSED[n] = (u.i, u.j)  # store information about the predecessor
 
@@ -48,18 +52,23 @@ class AStar:
         next_node = self.start  # if path not found, current start position is returned
         if self.goal in self.CLOSED:  # if path found
             next_node = self.goal
-            while self.CLOSED[next_node] != self.start:  # get node in the path with start node as a predecessor
+            while self.CLOSED[
+                next_node] != self.start:  # get node in the path with start node as a predecessor
                 next_node = self.CLOSED[next_node]
         return next_node
 
     def update_obstacles(self, obs, other_agents, n):
-        obstacles = np.transpose(np.nonzero(obs))  # get the coordinates of all obstacles in current observation
+        obstacles = np.transpose(
+            np.nonzero(obs))  # get the coordinates of all obstacles in current observation
         for obstacle in obstacles:
-            self.obstacles.add((n[0] + obstacle[0], n[1] + obstacle[1]))  # save them with correct coordinates
+            self.obstacles.add(
+                (n[0] + obstacle[0], n[1] + obstacle[1]))  # save them with correct coordinates
         self.other_agents.clear()  # forget previously seen agents as they move
-        agents = np.transpose(np.nonzero(other_agents))  # get the coordinates of all agents that are seen
+        agents = np.transpose(
+            np.nonzero(other_agents))  # get the coordinates of all agents that are seen
         for agent in agents:
-            self.other_agents.add((n[0] + agent[0], n[1] + agent[1]))  # save them with correct coordinates
+            self.other_agents.add(
+                (n[0] + agent[0], n[1] + agent[1]))  # save them with correct coordinates
 
 
 class Model:
@@ -67,33 +76,39 @@ class Model:
         self.agents = None
         self.lengthobs = 0
         self.actions = {tuple(GridConfig().MOVES[i]): i for i in
-                        range(len(GridConfig().MOVES))}  # make a dictionary to translate coordinates of actions into id
-        self.lst = []
+                        range(
+                            len(GridConfig().MOVES))}  # make a dictionary to translate coordinates of actions into id
+
     def act(self, obs, dones, positions_xy, targets_xy) -> list:
         if self.agents is None:
             self.lengthobs = len(dones)
-            self.lst = [0 for i in range(self.lengthobs)]
-            self.agents = [AStar() for _ in range(self.lengthobs)]  # create a planner for each of the agents
-        actions = self.lst
+            self.agents = [AStar() for _ in
+                           range(self.lengthobs)]  # create a planner for each of the agents
+        actions = []
         for k in range(self.lengthobs):
-            if positions_xy[k] == targets_xy[k]:  # don't waste time on the agents that have already reached their goals
-                actions[k]=0  # just add useless action to save the order and length of the actions
+            if positions_xy[k] == targets_xy[
+                k]:  # don't waste time on the agents that have already reached their goals
+                actions.append(
+                    0)  # just add useless action to save the order and length of the actions
                 continue
-            self.agents[k].update_obstacles(obs[k][0], obs[k][1], (positions_xy[k][0] - 5, positions_xy[k][1] - 5))
-            self.agents[k].compute_shortest_path(start=positions_xy[k], goal=targets_xy[k])
+            self.agents[k].update_obstacles(obs[k][0], obs[k][1],
+                                            (positions_xy[k][0] - 5, positions_xy[k][1] - 5))
+            self.agents[k].compute_shortest_path(start=positions_xy[k], goal=targets_xy[k],
+                                                 n=self.lengthobs)
             next_node = self.agents[k].get_next_node()
-            actions[k]=self.actions[(next_node[0] - positions_xy[k][0], next_node[1] - positions_xy[k][1])]
+            actions.append(
+                self.actions[(next_node[0] - positions_xy[k][0], next_node[1] - positions_xy[k][1])])
         return actions
 
 
 def main():
     # Define random configuration
-    grid_config = GridConfig(num_agents=32,  # количество агентов на карте
+    grid_config = GridConfig(num_agents=500,  # количество агентов на карте
                              size=64,  # размеры карты
                              density=0.3,  # плотность препятствий
                              seed=1,  # сид генерации задания
                              max_episode_steps=256,  # максимальная длина эпизода
-                             obs_radius=5,  # радиус обзора
+                             obs_radius=5,  # радиус обзорg
                              )
 
     env = gym.make("Pogema-v0", grid_config=grid_config)
@@ -105,14 +120,15 @@ def main():
     done = [False for k in range(len(obs))]
     solver = Model()
     steps = 0
+    st = time.time()
     while not all(done):
         # Используем AStar
         obs, reward, done, info = env.step(solver.act(obs, done,
                                                       env.get_agents_xy_relative(),
                                                       env.get_targets_xy_relative()))
         steps += 1
-        #print(steps, np.sum(done))
-
+        # print(steps, np.sum(done))
+    print(time.time() - st)
     # сохраняем анимацию и рисуем ее
     env.save_animation("render.svg", egocentric_idx=None)
 
