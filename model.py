26608776245525c44141c27255c25a201fd35690
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2022-05-30 19:35:30 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import time
+from random import randint, shuffle
 
 import numpy as np
 from heapq import heappop, heappush
 import gym
 from pogema.animation import AnimationMonitor
 from pogema import GridConfig
-from random import randint, shuffle, sample
 
+##########################################################################
 
 class Node:
     def __init__(self, coord: (int, int) = (0, 0), g: int = 0, h: int = 0):
@@ -25,7 +26,7 @@ class AStar:
     def __init__(self):
         self.start = (0, 0)
         self.goal = (0, 0)
-        self.max_steps = 5000  # due to the absence of information about the map size we need some other stop criterion
+        self.max_steps = 10000  # due to the absence of information about the map size we need some other stop criterion
         self.OPEN = list()
         self.CLOSED = dict()
         self.obstacles = set()
@@ -43,7 +44,7 @@ class AStar:
         heappush(self.OPEN, Node(self.start))
         u = Node()
         steps = 0
-        max_steps = min(max(self.max_steps - 12 * n, 20), self.max_steps)
+        max_steps = self.max_steps # min(max(self.max_steps - 12 * n, 20), self.max_steps)
         while self.OPEN and steps < max_steps and (u.i, u.j) != self.goal:
             u = heappop(self.OPEN)
             steps += 1
@@ -56,7 +57,11 @@ class AStar:
                     self.CLOSED[n] = (u.i, u.j)  # store information about the predecessor
                     if not h:
                         return
-
+    def check_new_pos(self, pos):
+        return pos in self.obstacles or pos in self.other_agents
+    def build(self):
+        self.isbuild = False
+        self.path.clear()
     def get_next_node(self):
         if not self.path:
             self.isbuild = False
@@ -70,6 +75,7 @@ class AStar:
                 next_node = self.CLOSED[next_node]
                 self.path.append(next_node)
             self.isbuild = True
+            next_node = self.path.pop()
         return next_node
 
     def update_obstacles(self, obs, other_agents, n):
@@ -88,12 +94,10 @@ class AStar:
             # save them with correct coordinates
 
 
-mxs = set()
 
 
 class Model:
     MAXSIZE = 10
-    MAXTIME = 5#650
 
     def __init__(self):
         self.starttime = time.time()
@@ -103,7 +107,7 @@ class Model:
         self.kx = [0, -1, 1, 0, 0]
         self.ky = [0, 0, 0, -1, 1]
         self.kxy = [(self.kx[i], self.ky[i]) for i in range(len(self.kx))]
-        self.failpair = {(1, 2), (2, 1), (3, 4), (4, 3)}
+        self.failpair = {(1, 2), (2, 1), (3, 4), (4, 3), (0, 0)}
         self.agents = None
         self.key = False
         self.indexs = list()
@@ -112,18 +116,13 @@ class Model:
         self.actions = {tuple(GridConfig().MOVES[i]): i for i in
                         range(
                             len(GridConfig().MOVES))}  # make a dictionary to translate coordinates of actions into id
-
-    def deltatime(self) -> bool:
-        with open("time.txt", mode='w+') as file:
-            s = file.readline()
-            if s:
-                start_time = float(s)
-            else:
-                start_time=time.time()
-                file.write(str(start_time))
-            return (time.time() - start_time) > self.MAXTIME
+        self.count = 0
 
     def act(self, obs, dones, positions_xy, targets_xy) -> list:
+        #print(positions_xy)
+        self.count += 1
+        if self.count == 6:
+            pass # breakpoint()
         if self.agents is None:
             self.lengthobs = len(dones)
             self.agents = [AStar() for _ in range(self.lengthobs)]
@@ -156,16 +155,18 @@ class Model:
                 actions[k] = 0
                 self.postmove[self.key & 1][k] = 0
             else:
+                if positions_xy == [(20, 7)]:
+                    pass # breakpoint()
                 if not actions[k] or self.postmove[self.key & 1][k] == actions[k] and \
                         (actions[k], self.postmove[(self.key + 1) & 1][k]) not in self.failpair:
-                    self.agents[k].isbuild = False
+                    self.agents[k].build()
 
                     self.agents[k].update_obstacles(obs[k][0], obs[k][1],
                                                     (positions_xy[k][0] - 5, positions_xy[k][1] - 5))
                     self.agents[k].compute_shortest_path(start=positions_xy[k], goal=targets_xy[k])
 
                     next_node = self.agents[k].get_next_node()
-                    actions[k] = self.actions.get((next_node[0] - positions_xy[k][0],
+                    indx = actions[k] = self.actions.get((next_node[0] - positions_xy[k][0],
                                                    next_node[1] - positions_xy[k][1]), 0)
 
                 shuffle(self.kxy)
@@ -216,7 +217,7 @@ def main():
                                                       env.get_targets_xy_relative()))
         steps += 1
         # print(steps, np.sum(done))
-    #print((time.time() - st))
+    print((time.time() - st))
     # сохраняем анимацию и рисуем ее
     env.save_animation("render.svg", egocentric_idx=None)
     # print(max(mxs))
